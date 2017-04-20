214021b6717c8e29e93a457068eec79fe20fb07b
Dvessen83 <d.v.essen@gmail.com>
2017-04-20 21:02:57 +0200

    algorithm wip

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -11,10 +11,41 @@ class TeamsController < ApplicationController
   end
 
   def new
-    @team = current_user.teams.build
+    if current_user.admin == true
+      @team = Team.new
+    else
+      flash[:alert] = "You are unable to perform this action!"
+      redirect_to root_path
+    end
   end
 
   def create
+
+    team = params[:team]
+    date = Date.new team["date(1i)"].to_i, team["date(2i)"].to_i, team["date(3i)"].to_i
+    User.all.each do |user|
+      Team.create(date: date, member1: user, member2: available_user(user,date))
+    end
+    flash[:notice] = "Teams generated for #{date}"
+    redirect_to teams_path
+  end
+
+  private
+
+  def available_user(member1,date)
+    users = User.all
+    todays_teams = Team.all.select{ |team| team.date == date }
+
+    available_users = users.reject{ |user|
+      user == member1 && todays_teams.each do |t|
+        t.member1
+      end &&
+      todays_teams.each do |t|
+        t.member2
+      end
+    }
+
+    available_users.sample
   end
 
 end
