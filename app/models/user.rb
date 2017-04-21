b1121290f9b352a1a6fa3c90d1fc3f93c0b7a768
Dvessen83 <d.v.essen@gmail.com>
2017-04-21 10:39:48 +0200

    Cleaned up some code, removed teams from seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ApplicationRecord
   has_many :teams, class_name: "User", foreign_key: "member1", dependent: :destroy
-  # has_many :member1s, class_name: "User", foreign_key: "member1"
-  # has_many :member2s, class_name: "User", foreign_key: "member2"
-
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,3 @@ bill = User.create(email: "bill@user.com", password: "abcd1234", name: "Bill", a
 peter = User.create(email: "peter@user.com", password: "abcd1234", name: "Peter", admin: false)
 scott = User.create(email: "scott@user.com", password: "abcd1234", name: "Scott", admin: false)
 richard = User.create(email: "richard@user.com", password: "abcd1234", name: "Richard", admin: false)
-
-team1 = Team.create(date: "22/4/2017", member1_id: 1, member2_id: 2)
-team2 = Team.create(date: "23/4/2017", member1_id: 1, member2_id: 3)
-team3 = Team.create(date: "24/4/2017", member1_id: 2, member2_id: 3)
